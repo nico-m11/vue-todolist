cf46da6d3db81c0cb2b4e6aecf420d572ad03a3f
Nicola <nicolamelito.11@gmail.com>
2020-11-19 19:55:24 +0100

    Completato il Bonus
    non ho cambiato i nomi come riferito da Alessandro

--- js/script.js
+++ js/script.js
@@ -3,15 +3,20 @@ var app = new Vue ({
   data: {
     testoInput: '',
     inputUtente: [],
+    active: []
   },
 
   methods: {
       inputUtenteArray: function () {
         this.inputUtente.push(this.testoInput)
         this.testoInput = '';
+        myActive = false;
       },
       cancellaInput: function (cancIndex) {
         this.inputUtente.splice(cancIndex, 1)
+      },
+      actived: function (index) {
+        this.$set(this.active, index, !this.active[index]);
       },
     },
 });
